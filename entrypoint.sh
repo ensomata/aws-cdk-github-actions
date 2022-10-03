7b618062e0c11ca6bf3922a5c808e13064480dc2
phil-ensomata <105312524+phil-ensomata@users.noreply.github.com>
2022-10-03 15:21:58 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -60,6 +60,42 @@ function installPipRequirements(){
 	fi
 }
 
+function runCdkAll(){
+	echo "Run cdk ${INPUT_CDK_SUBCOMMAND} ${*} --all"
+	set -o pipefail
+	cdk ${INPUT_CDK_SUBCOMMAND} ${*} --all 2>&1 | tee output.log
+	exitCode=${?}
+	set +o pipefail
+	echo ::set-output name=status_code::${exitCode}
+	output=$(cat output.log)
+
+	commentStatus="Failed"
+	if [ "${exitCode}" == "0" ]; then
+		commentStatus="Success"
+	elif [ "${exitCode}" != "0" ]; then
+		echo "CDK subcommand ${INPUT_CDK_SUBCOMMAND} for stack ${INPUT_CDK_STACK} has failed. See above console output for more details."
+		exit 1
+	fi
+
+	if [ "$GITHUB_EVENT_NAME" == "pull_request" ] && [ "${INPUT_ACTIONS_COMMENT}" == "true" ]; then
+		commentWrapper="#### \`cdk ${INPUT_CDK_SUBCOMMAND}\` ${commentStatus}
+<details><summary>Show Output</summary>
+
+\`\`\`
+${output}
+\`\`\`
+
+</details>
+
+*Workflow: \`${GITHUB_WORKFLOW}\`, Action: \`${GITHUB_ACTION}\`, Working Directory: \`${INPUT_WORKING_DIR}\`*"
+
+		payload=$(echo "${commentWrapper}" | jq -R --slurp '{body: .}')
+		commentsURL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
+
+		echo "${payload}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${commentsURL}" > /dev/null
+	fi
+}
+
 function runCdk(){
 	echo "Run cdk ${INPUT_CDK_SUBCOMMAND} ${*} \"${INPUT_CDK_STACK}\""
 	set -o pipefail
@@ -102,7 +138,11 @@ function main(){
 	installTypescript
 	installAwsCdk
 	installPipRequirements
-	runCdk ${INPUT_CDK_ARGS}
+	if [ "${INPUT_CDK_ALL}" == "true" ]; then
+		runCdkAll ${INPUT_CDK_ARGS}
+	else
+		runCdk ${INPUT_CDK_ARGS}
+	fi
 }
 
 main
